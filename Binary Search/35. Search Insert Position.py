f53a0969999909d74d157e86a3d6d3984f0570f2
Shoreline <joseftien@gmail.com>
2022-01-07 01:50:15 -0800

    Update

---
+++ Binary Search/35. Search Insert Position.py
@@ -0,0 +1,19 @@
+# Binary search
+# low + high may cause integer overflow for some languages, but not python3
+
+# If target is not to be found: we end up with low < high and nums[high] < target < nums[left]. In this case,
+# target shall be inserted to nums[left]
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            # mid = (low + high) // 2
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
