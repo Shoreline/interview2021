bf3137e86b081b3c1c0a226145e2d29e933350b2
Shoreline <joseftien@gmail.com>
2022-12-25 17:13:01 -1000

    Update Combination Sum II.py

--- Backtracking/Combination Sum II.py
+++ Backtracking/Combination Sum II.py
@@ -2,25 +2,49 @@
 # candidates[i] == candidates[i - 1]: continue In one for loop that i in [pos, len(candidates)), for repeated
 # elements only proceed to process the first one of each repeated set. Because processing any one of a set of
 # repeated elements is equivalent. Note that this way elements of the same value can still be in res[].
+
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
         candidates.sort()
+        res = []
 
-        def dfs(target: int, pos: int, tmp: list[int]):
+        def dfs(pos, target, tmp):
             if target == 0:
                 res.append(tmp[:])
                 return
             if target < 0:
-                return
-
+                return 
             for i in range(pos, len(candidates)):
+                # when candidates[i] == candidates[i-1]
+                # Picking candidates[i] and candidates[i-1] are equivalent and cause duplicated answer
                 if i > pos and candidates[i] == candidates[i - 1]:
                     continue
-
                 tmp.append(candidates[i])
-                dfs(target - candidates[i], i + 1, tmp)
+                dfs(i+1, target - candidates[i], tmp)
                 tmp.pop()
-
-        dfs(target, 0, [])
+        
+        dfs(0, target, [])
         return res
+
+# class Solution:
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = []
+#         candidates.sort()
+
+#         def dfs(target: int, pos: int, tmp: list[int]):
+#             if target == 0:
+#                 res.append(tmp[:])
+#                 return
+#             if target < 0:
+#                 return
+
+#             for i in range(pos, len(candidates)):
+#                 if i > pos and candidates[i] == candidates[i - 1]:
+#                     continue
+
+#                 tmp.append(candidates[i])
+#                 dfs(target - candidates[i], i + 1, tmp)
+#                 tmp.pop()
+
+#         dfs(target, 0, [])
+#         return res
