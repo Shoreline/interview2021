b7d98ccace2a29ea1bd5c4c6c765e70e9239cb52
Shoreline <joseftien@gmail.com>
2022-12-26 10:01:09 -1000

    Update Number of Islands.py

--- Backtracking/Number of Islands.py
+++ Backtracking/Number of Islands.py
@@ -1,23 +1,39 @@
 # Backtracking
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def mark(i: int, j: int):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == '0':
-                return
-            grid[i][j] = '0'
-            mark(i - 1, j)
-            mark(i + 1, j)
-            mark(i, j - 1)
-            mark(i, j + 1)
-            return
 
+        def paint(val, new_val, x, y):
+            if 0 <= x < len(grid) and 0<= y < len(grid[0]) and grid[x][y] == val:
+                grid[x][y] = new_val
+                for r, c in ([x+1, y], [x-1, y], [x, y+1], [x, y-1]):
+                    paint(val, new_val, r, c)
+        
         res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
+                if grid[i][j] == "1":
                     res += 1
-                    mark(i, j)
+                    paint("1", "0", i, j)
         return res
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         def mark(i: int, j: int):
+#             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == '0':
+#                 return
+#             grid[i][j] = '0'
+#             mark(i - 1, j)
+#             mark(i + 1, j)
+#             mark(i, j - 1)
+#             mark(i, j + 1)
+#             return
+
+#         res = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j] == '1':
+#                     res += 1
+#                     mark(i, j)
+#         return res
 
 # class Solution:
 #     def numIslands(self, grid: List[List[str]]) -> int:
