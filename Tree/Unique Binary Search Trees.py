9fd1856ff240620f224d968f74e196dc8ace231a
Shoreline <joseftien@gmail.com>
2022-12-27 11:28:42 -1000

    Update Unique Binary Search Trees.py

--- Tree/Unique Binary Search Trees.py
+++ Tree/Unique Binary Search Trees.py
@@ -14,15 +14,27 @@
 # Since nodes of this problem is from 1 ~ n. The possible node number of left tree is from 0 to n-1, say k.
 # And meanwhile the possible node number of the right tree will be n-1-k (n - 1 - k: minus the root, then minus the node count in left tree )
 #  */
+
 class Solution:
     def numTrees(self, n: int) -> int:
         dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 1
 
-        for i in range(2, n + 1):
-            for k in range(i):  # use k (a number) as the root. k is [0,i).
-                dp[i] += dp[k] * dp[
-                    i - k - 1]  # numTrees of the two sub trees. Note that the maximum number of nodes in each subtree is i-1 (there has to be a root node)
-
+        for i in range(2, n+1): # number of nodes: from 2 to n
+            for k in range(i): # number of left tree nodes: from 0 to i - 1 (there has to be one root node, so i - 1 instead of i)
+                dp[i] += dp[k] * dp[i - 1 - k]
+        
         return dp[-1]
+# class Solution:
+#     def numTrees(self, n: int) -> int:
+#         dp = [0] * (n + 1)
+#         dp[0] = 1
+#         dp[1] = 1
+
+#         for i in range(2, n + 1):
+#             for k in range(i):  # use k (a number) as the root. k is [0,i).
+#                 dp[i] += dp[k] * dp[
+#                     i - k - 1]  # numTrees of the two sub trees. Note that the maximum number of nodes in each subtree is i-1 (there has to be a root node)
+
+#         return dp[-1]
