6147eaabfcdafd02fb7aa168e850001fcd61e643
Shoreline <joseftien@gmail.com>
2023-01-10 23:16:27 -0800

    Update

--- Sorting/Kth Smallest Element in a Sorted Matrix.py
+++ Sorting/Kth Smallest Element in a Sorted Matrix.py
@@ -4,33 +4,37 @@
 # Nature thought: max heap. But this time the input array is not random, but sorted in some way.
 # So, our thought is to use a min heap, and pop k elements.
 
-# the total time complexity for this algorithm comes down to be O(X+Klog(X)) where X is min(K, N). N: len(matrix), the number of sorted lists
+# the total time complexity for this algorithm comes down to be O(X+Klog(X)) where X is min(K, N). N: len(matrix), the
+# number of sorted lists.
 # Since all rows/columns are already sorted, only need to check a subset of elements in matrix
 
 # min-heap will contain a triplet of (val, row, column)
-# It's important to know what row and column an element belongs to. Without knowing that, we won't be able to move forward in that particular list. So, apart from adding an element to the heap, we also need to add its row and column number.
+# It's important to know what row and column an element belongs to. Without knowing that, we won't be able to move
+# forward in that particular list. So, apart from adding an element to the heap, we also need to add its row and column
+# number.
 
 # Say the minimum triplet in the heap is (v, r, c), then when it is pop out, which triplet to add to the heap?
-#   - The next triplet doesn't HAVE to be the next minium element in matrix. It shall be the next POSSIBLE minium element
+#   - The next triplet doesn't HAVE to be the next minium element in matrix. It shall be the next POSSIBLE min element
 #   - It shall be (v2, r, c+1).  (Why not (v2', r+1, c)? actually that may also work)
 #   - No need to ever increment r. The heappush() will do that while finding the minimum triplet
 
 import heapq
 
-
+# Maintain a min_heap that always has the smallest element.
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        min_heap = []  # Its size does not to be k. Our goal is not to KEEP k smallest elements in it. But to POP smallest element K times.
+        # Its size does not need to be k. Our goal is not to KEEP the k smallest elements in it. But to POP smallest
+        # element K times.
+        min_heap = []
         # Also, the size of min_heap doesn't need to be a constant
 
         for row in range(min(k, len(matrix))):
             min_heap.append((matrix[row][0], row, 0))  # triplet: val, row, column
         # heapq.heapify(min_heap)
 
-        # while havn't poped smallest element k times
-        while k > 0:
+        # while we haven't poped the smallest element k times
+        for i in range(k):
             val, row, column = heapq.heappop(min_heap)  # not triplet = heapq.heappop(min_heap)
-            k -= 1
             if column < len(matrix) - 1:
                 heapq.heappush(min_heap, (matrix[row][column + 1], row, column + 1))
 
--- Sorting/Largest Number.py
+++ Sorting/Largest Number.py
@@ -1,57 +1,35 @@
 # sorting problem.
 # sort n1 and n2 by each digit
 # - can also be done by let x and y be numbers in string format. And compare their concatenated results
-# Note that python3 does not support cmp = compare in sort() anymore (can workaround by import functools)
+# Note that python3 does not support cmp = compare in sort() anymore (can work around by import functools)
+# This solution uses the build-in sort(). We can also write our own quicksort function.
 
-class LargerNumKey(str):
-    def __lt__(x: str, y: str):
-        return x + y > y + x  # x and y are numbers in string format. Here are compare their concatenated results
+import functools
 
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
-        # return '0' if largest_num[0] == '0' else largest_num # largest_num can be "00"
+        def comparator(x, y):
+            return 1 if x + y > y + x else -1
 
-        nums_str = [str(num) for num in nums]
-        nums_str.sort(key=LargerNumKey)
-        if nums_str[0] == '0':
-            return '0'
-        else:
-            return ''.join(nums_str)
+        str_nums = [str(num) for num in nums]
+        str_nums.sort(key=functools.cmp_to_key(comparator), reverse=True)
+        return '0' if str_nums[0] == '0' else ''.join(str_nums)
 
-# public class LargestNumber {
-#     public class Solution {
-# 	public String largestNumber(int[] nums) {
-# 	    if (nums == null || nums.length == 0) {
-# 		return "";
-# 	    }
-
-# 	    Integer[] nums2 = new Integer[nums.length];
-# 	    for (int i = 0; i < nums.length; i++) {
-# 		nums2[i] = nums[i];
-# 	    }
-
-# 	    Arrays.sort(nums2, new Comparator<Integer>() {
-# 		@Override
-# 		public int compare(Integer i1, Integer i2) {
-# 		    String s1 = String.valueOf(i1);
-# 		    String s2 = String.valueOf(i2);
-# 		    return -(s1 + s2).compareTo(s2 + s1);
-# 		}
-# 	    });
-
-# 	    StringBuilder res = new StringBuilder();
-# 	    for (int i = 0; i < nums2.length; i++) {
-# 		// avoid cases like "00".
-# 		if (res.length() == 0 && nums2[i] == 0 && i < nums2.length - 1 && nums2[i] == nums2[i + 1]) {
-# 		    continue;
-# 		}
-# 		res.append(String.valueOf(nums2[i]));
-# 	    }
-
-# 	    return res.toString();
-# 	}
-#     }
-
-# }
+# Don't use functools
+# class LargerNumKey(str):
+#     def __lt__(x: str, y: str):
+#         return x + y > y + x  # x and y are numbers in string format. Here are compare their concatenated results
+#
+#
+# class Solution:
+#     def largestNumber(self, nums: List[int]) -> str:
+#         # largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+#         # return '0' if largest_num[0] == '0' else largest_num # largest_num can be "00"
+#
+#         nums_str = [str(num) for num in nums]
+#         nums_str.sort(key=LargerNumKey)
+#         if nums_str[0] == '0':  # corner case: when all nums elements are 0
+#             return '0'
+#         else:
+#             return ''.join(nums_str)
