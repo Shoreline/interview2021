fc9202761856417ddcf458ddf80a7648ded47ac7
Shoreline <joseftien@gmail.com>
2023-05-01 04:19:23 -0700

    Update

---
+++ Array/Jump Game III.py
@@ -0,0 +1,8 @@
+# There is one arr[des] == 0, see if it can be reached from arr[start]
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        if 0 <= start < len(arr) and arr[start] >= 0:
+            arr[start] = -arr[start]
+            return arr[start] == 0 or self.canReach(arr, start - arr[start]) or self.canReach(arr, start + arr[start])
+
+        return False
